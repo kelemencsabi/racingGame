d0f2ebf83932809c9479fd9e933968955cabe263
Csabi <csabykelemen@gmail.com>
2019-06-19 20:01:37 +0300

    studied encapsulation

--- src/main/java/org/fasttrackit/App.java
+++ src/main/java/org/fasttrackit/App.java
@@ -9,39 +9,39 @@ public class App {
         engine1.manufacturer = "Renault";
 
         Car car = new Car(engine1);
-        car.name = "Dacia";
-        car.color = "red";
+        car.setName("dacia");
+        car.setColor("red");
         car.doorCount = 4;
-        car.running = true;
-        car.maxSpeed = 120;
-        car.fuelLevel = 50;
-        car.mileage = 8;
+        car.setRunning(true);
+        car.setMaxSpeed(120);
+        car.setFuelLevel(50);
+        car.setMileage(8);
 
         Engine engine2 = new Engine();
         engine2.capacity = 120;
         engine2.manufacturer = "Opel";
 
         Car car2 = new Car(engine2);
-        car2.name = "Opel";
-        car2.color = "black";
+        car2.setName("Opel");
+        car2.setColor("blue");
         car2.doorCount = 4;
-        car2.mileage = 9.8;
-        car2.maxSpeed = 180;
-        car2.running = false;
+        car2.setMaxSpeed(180);
+        car2.setFuelLevel(60);
+        car2.setRunning(false);
 
 
 
         Car car3 = car;
-        System.out.println(car.name);
+        System.out.println(car.getName());
 
 
         System.out.println("changing values...");
-        car.name = "Dacia Logan";
+        car.setName("Dacia Logan");
 
-        System.out.println(car.name);
+        System.out.println(car.getName());
 
 
-        System.out.println("max speed: " + car.maxSpeed);
+        System.out.println("max speed: " + car.getMaxSpeed());
 
         System.out.println( "Engine manufacturer: " + car.engine.manufacturer);
 
