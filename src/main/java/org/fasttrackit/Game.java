3c66a974aacccecdd3828c0b0ced0a7d84d3cfff
Csabi <csabykelemen@gmail.com>
2019-07-10 20:14:52 +0300

    Getting input from user

--- src/main/java/org/fasttrackit/Game.java
+++ src/main/java/org/fasttrackit/Game.java
@@ -1,15 +1,55 @@
 package org.fasttrackit;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
 public class Game {
-    private Vehicle competiror1;
-    private Vehicle competitor2;
+
+    Scanner input = new Scanner(System.in);
+    DecimalFormat df2 =new DecimalFormat("#.##");
 
     private Track[] tracks = new Track[3];
+    private List<Vehicle> competitors = new ArrayList<>();
 
     public void start() {
         initializeTracks();
         displayTracks();
+        System.out.println("enter competitor count ");
+        int competitorCount =input.nextInt();
+        for(int i=0;i<competitorCount;i++){
+            addCompetitor();
+        }
+        displayCompetitors();
     }
+
+    private void addCompetitor() {
+        Vehicle vehicle = new Vehicle();
+        System.out.println("Enter competitor name:");
+        vehicle.setName(input.next());
+        System.out.println("Enter fuel level:");
+        vehicle.setFuelLevel(input.nextInt());
+        System.out.println("Enter max speed:");
+        vehicle.setMaxSpeed(input.nextInt());
+        vehicle.setMileage(ThreadLocalRandom.current().nextDouble(5, 10));
+        competitors.add(vehicle);
+    }
+
+    private void displayCompetitors() {
+        System.out.println("todays competitors are:");
+        for (int i = 0; i < competitors.size(); i++) {
+            ;
+            if (competitors.get(i) != null) {
+                ;
+                System.out.println(competitors.get(i).getName());
+                System.out.println("-mileage:" + df2.format(competitors.get(i).getMileage()));
+
+            }
+        }
+    }
+
 
     private void initializeTracks() {
         Track city = new Track();
@@ -27,7 +67,7 @@ public class Game {
         System.out.println("available tracks:");
         for (int i = 0; i < tracks.length; i++) {
             if (tracks[i] != null)
-            System.out.println((i + 1)+ " " + tracks[i].getName());
+                System.out.println((i + 1) + " " + tracks[i].getName());
         }
     }
 }
